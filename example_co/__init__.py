adbf03fb89a95c683da84a94106d02536a80ebe2
nikki everett <nikki@dagsterlabs.com>
2025-03-02 23:50:47 -0600

    fix
    Signed-off-by: nikki everett <nikki@dagsterlabs.com>

--- example_co/__init__.py
+++ example_co/__init__.py
@@ -1,1 +1,9 @@
+from dagster import Definitions, load_assets_from_modules
 
+from .assets import etl  # noqa: TID252
+
+all_assets = load_assets_from_modules([etl])
+
+defs = Definitions(
+    assets=all_assets,
+)
